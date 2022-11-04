335eee4f945b6c7fec34a67065ac373e3d69b1df
Waseek Ahmed <58625384+WaseekSenju@users.noreply.github.com>
2022-11-04 15:22:13 +0500

    better approach

--- Playground/test.cpp
+++ Playground/test.cpp
@@ -7,14 +7,13 @@ public:
     void shiftAllZerosToEnd(vector<int> &nums)
     {
         int length = nums.size();
-
-        for (int i = 0; i < length / 2; i++)
+        int j = 0;
+        for (int i = 0; i < length; i++)
         {
-            if (nums[i] == 0)
+            if (nums[i] != 0)
             {
-                int temp = nums[i];
-                nums[i] = nums[length - i - 1];
-                nums[length - i - 1] = temp;
+                swap(nums[j], nums[i]);
+                j++;
             }
         }
         for (int i = 0; i < length; i++)
@@ -26,6 +25,6 @@ public:
 int main()
 {
     Solution s1 = Solution();
-    std::vector<int> vector = {0, 0, 0, 0, 1, 1, 1, 1, 3, 3, 3, 4};
+    std::vector<int> vector = {0, 2, 0, 0, 4, 0, 1, 1, 3, 3, 3, 4};
     s1.shiftAllZerosToEnd(vector);
 }
