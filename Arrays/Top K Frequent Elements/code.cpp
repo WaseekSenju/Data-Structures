18ae8d93c89ed7fb4a65183178bd75d0ad9d8e43
WaseekSenju <waseeq69ahmed@gmail.com>
2023-03-01 13:51:36 +0500

    top K using bucket sort and sorting the map

--- Arrays/Top K Frequent Elements/code.cpp
+++ Arrays/Top K Frequent Elements/code.cpp
@@ -40,3 +40,32 @@ public:
     }
 };
 //--------------Using Bucket Sort
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            m[nums[i]]++;
+        }
+        
+        vector<vector<int>> buckets(n + 1);
+        for (auto it = m.begin(); it != m.end(); it++) {
+            buckets[it->second].push_back(it->first);
+        }
+        
+        vector<int> result;
+        
+        for (int i = n; i >= 0; i--) {
+            if (result.size() >= k) {
+                break;
+            }
+            if (!buckets[i].empty()) {
+                result.insert(result.end(), buckets[i].begin(), buckets[i].end());
+            }
+        }
+        
+        return result;
+    }
+};
